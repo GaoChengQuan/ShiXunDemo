91057de9b91c3e071c363027758735b2b92beadc
GaoChengQuan <529390053@qq.com>
2017-09-11 14:31:54 +0800

    Scale动画

--- src/com/qd/demo/MainActivity.java
+++ src/com/qd/demo/MainActivity.java
@@ -4,13 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
+	private ImageView mImageView;
+	private Button mScale;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mImageView = (ImageView) findViewById(R.id.imageView);
+		mScale = (Button) findViewById(R.id.scale);
+		mScale.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale);
+				mImageView.startAnimation(animation);
+			}
+		});
 	}
 
 	
