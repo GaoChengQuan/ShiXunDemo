a4bd4448fcfa65daf074700674b971eab65c73d8
GaoChengQuan <529390053@qq.com>
2017-09-11 15:35:58 +0800

    组合动画

--- src/com/qd/demo/MainActivity.java
+++ src/com/qd/demo/MainActivity.java
@@ -1,11 +1,10 @@
 package com.qd.demo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
 	private Button mAlpha;
 	private Button mRotate;
 	private Button mTranslate;
+	private Button mContinue;
 	
 
 	@Override
@@ -60,6 +60,39 @@ public class MainActivity extends ActionBarActivity {
 			public void onClick(View arg0) {
 				Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.translate);
 				mImageView.startAnimation(animation);
+			}
+		});
+		
+		mContinue = (Button) findViewById(R.id.continue1);
+		mContinue.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Animation animation1 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.translate);
+				final Animation animation2 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotate);
+				//先给图片设置translate动画
+				mImageView.startAnimation(animation1);
+			    //设置动画的监听事件
+				animation1.setAnimationListener(new AnimationListener() {
+					
+					@Override
+					public void onAnimationStart(Animation arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onAnimationRepeat(Animation arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onAnimationEnd(Animation arg0) {
+						//translate动画结束之后设置旋转动画
+						mImageView.startAnimation(animation2);
+					}
+				});
 			}
 		});
 	}
